3009645716141565a8d7c12cea9ea5849c4d0522
Cristian Marletta <sedetiam.io@gmail.com>
2017-12-04 19:08:08 +0100

    added flex layout, added media queries breakpoints

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 import Hello from './components/Hello/Hello.jsx';
 import Portfolio from './components/Portfolio/Portfolio.jsx';
 //modules
+import style from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={style.App}>
         <Hello />
         <Portfolio />
       </div>
