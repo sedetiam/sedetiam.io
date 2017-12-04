4685264fa8651ddd504bd80886b0c0b192918613
Cristian Marletta <sedetiam.io@gmail.com>
2017-12-04 20:04:24 +0100

    fixed media query layout change on portrait orientation

--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import css from './Hello.module.css';
+import style from './Hello.module.css';
 
 class Hello extends Component {
   render() {
     return (
-      <article className={css.container}>
+      <article className={style.container}>
         <header>
-          <h1 className={css.hello}>Hello :)</h1>
+          <h1 className={style.hello}>Hello :)</h1>
         </header>
-        <p className={css.presentation}>
+        <p className={style.presentation}>
           my name is Cristian Marletta<br />
           I am a software engineer and this is my portfolio<br />
           Here you can find some of my works &#10084;
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import css from './Portfolio.module.css';
+import style from './Portfolio.module.css';
 
 class Portfolio extends Component {
   render() {
     return (
-      <article className={css.container}>
-        <p className={css.presentation}>portfolio placeholder</p>
+      <article className={style.container}>
+        <p className={style.presentation}>portfolio placeholder</p>
       </article>
     );
   }
